e8c5baf57e04e599e0fb9fdc44843d24a31b72a7
Ronan Daunt <z5487303@ad.unsw.edu.au>
2026-08-14 11:54:27 +1000

    Updated maze location of robot each movement

--- Week12_Code/week_12/MazeNavigator.hpp
+++ Week12_Code/week_12/MazeNavigator.hpp
@@ -16,7 +16,12 @@ public:
     MazeNavigator(Maze& maze, MotionController& motion) : maze(maze), motion(motion) {}
 
     // 4.1 - given a string of commands, executes the path and returns true if successful
-    bool executePath (const char* path) {
+    bool executePath (const char* path, uint8_t startRow, uint8_t startCol, mtrn3100::Maze::Direction startDir) {
+
+        currRow = startRow;
+        currCol = startCol;
+        currDir = startDir;
+
         for (int i = 0; path[i] != '\0'; i++) {
             bool success = executeCommand(path[i]);
 
@@ -49,14 +54,18 @@ private:
         switch (cmd) {
             case 'f' :
                 success = motion.forwardOneCell();
+                updateLocationForward();
                 break;
             
             case 'l':
                 success = motion.turnLeft90();
+                updateLocationLeftTurn();
                 break;
 
             case 'r':
                 success = motion.turnRight90();
+                updateLocationRightTurn();
+
                 break;
             default:
                 Serial.print("Unknown command");
@@ -65,11 +74,65 @@ private:
         return success;
     }
 
+    // Updates the cell location going forward one cell based on direction
+    void updateLocationForward() {
+        
+        switch(currDir) {
+            case Maze::NORTH:
+                currRow--;
+                break;
+            case Maze::SOUTH: 
+                currRow++;
+                break;
+            case Maze::WEST:
+                currCol--;
+                break;
+            case Maze::EAST:
+                currCol++;
+                break;
+        }
+    }
+
+    void updateLocationRightTurn() {
+        switch(currDir) {
+            case Maze::NORTH:
+                currDir = Maze::EAST;
+                break;
+            case Maze::SOUTH: 
+                currDir = Maze::WEST;
+                break;
+            case Maze::WEST:
+                currDir = Maze::NORTH;
+                break;
+            case Maze::EAST:
+                currDir = Maze::SOUTH;
+                break;
+        }
+    }
+
+    void updateLocationLeftTurn() {
+        switch(currDir) {
+            case Maze::NORTH:
+                currDir = Maze::WEST;
+                break;
+            case Maze::SOUTH: 
+                currDir = Maze::EAST;
+                break;
+            case Maze::WEST:
+                currDir = Maze::SOUTH;
+                break;
+            case Maze::EAST:
+                currDir = Maze::NORTH;
+                break;
+        }
+    }
+
     Maze& maze;
     MotionController& motion;
 
     uint8_t currRow;
     uint8_t currCol;
     Maze::Direction currDir;
+    
 };
 }
